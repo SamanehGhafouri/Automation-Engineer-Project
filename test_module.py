039fd068c4067c99350ed03ecec20d75e1a540c9
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-08-05 13:20:58 -0400

    parametrized tests

---
+++ test_module.py
@@ -0,0 +1,26 @@
+import json
+import requests
+from parameterized import parameterized
+
+url = 'https://census-toy.nceng.net/prod/toy-census'
+
+
+def read_json(json_f):
+    with open(json_f, "r") as f:
+        data_obj = json.load(f)
+    return data_obj
+
+
+class Tests:
+
+    @parameterized.expand([
+        'test_data_1/gender_10_users.json',
+        'test_data_1/gender_5_users.json'
+    ])
+    def test_gender(self, base):
+        data = read_json(base)
+        response = requests.post(url, json=data["data"])
+        assert data["expected_gender"] == response.json()
+
+
+
