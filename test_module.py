448e7b606b480bacaa296e5f492938d98a1042dc
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-08-06 16:28:00 -0400

    13 tests passing 2 failed because CountByCountry not in descending order. Total 43 tests passing 2 failed.

--- test_module.py
+++ test_module.py
@@ -12,6 +12,7 @@ def read_json(json_f):
 
 
 class Tests:
+
     # Testing if the length of expected = length of actual
     @parameterized.expand([
         'test_data/gender_country_password_10_user.json',
@@ -66,6 +67,64 @@ class Tests:
 
         expected = set()
         for obj in data["expected_gender"]:
+            tuples = tuple([v for v in obj.values()])
+            expected.add(tuples)
+        assert expected == actual
+
+    # Testing if the length of expected = length of actual
+    @parameterized.expand([
+        'test_data/gender_country_password_10_user.json',
+        'test_data/gender_country_password_5_user.json',
+        'test_data/gender_country_password_1_user.json',
+        'test_data/gender_country_password_0_users.json',
+        'test_data/gender_country_password_15_user.json'
+    ])
+    def test_len_count_by_country(self, base):
+        data = read_json(base)
+        data["data"]["actionType"] = "CountByCountry"
+        response = requests.post(url, json=data["data"])
+        if len(response.json()) != len(data["expected_country"]):
+            assert False
+        assert True
+
+    # Testing if the values are in descending order
+    @parameterized.expand([
+        'test_data/gender_country_password_10_user.json',
+        'test_data/gender_country_password_5_user.json',
+        'test_data/gender_country_password_1_user.json',
+        'test_data/gender_country_password_0_users.json',
+        'test_data/gender_country_password_15_user.json'
+    ])
+    def test_count_by_country_values_descending(self, base):
+        data = read_json(base)
+        data["data"]["actionType"] = "CountByCountry"
+        response = requests.post(url, json=data["data"])
+
+        actual = []
+        for obj in response.json():
+            actual.append(obj["value"])
+        assert sorted(actual, reverse=True) == actual
+
+    # Testing if (key, value) in expected is equal to (key, value) in actual
+    @parameterized.expand([
+        'test_data/gender_country_password_10_user.json',
+        'test_data/gender_country_password_5_user.json',
+        'test_data/gender_country_password_1_user.json',
+        'test_data/gender_country_password_0_users.json',
+        'test_data/gender_country_password_15_user.json'
+    ])
+    def test_count_by_country_name_value(self, base):
+        data = read_json(base)
+        data["data"]["actionType"] = "CountByCountry"
+        response = requests.post(url, json=data["data"])
+
+        actual = set()
+        for obj in response.json():
+            tuples = tuple([v for v in obj.values()])
+            actual.add(tuples)
+
+        expected = set()
+        for obj in data["expected_country"]:
             tuples = tuple([v for v in obj.values()])
             expected.add(tuples)
         assert expected == actual
