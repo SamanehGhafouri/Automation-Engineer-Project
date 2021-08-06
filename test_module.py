7bd491b79dd26177cbb57671fab4a8cee9d3084c
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-08-05 20:47:58 -0400

    2 failed tests and 6 passing

--- test_module.py
+++ test_module.py
@@ -18,7 +18,7 @@ class Tests:
         'test_data_1/gender_5_users.json',
         'test_data_1/gender_1_user.json',
         # 'test_data_1/gender_0_users.json',
-        # 'test_data_1/gender_15_users.json'
+        'test_data_1/gender_15_users.json'
     ])
     def test_gender(self, base):
         data = read_json(base)
@@ -31,7 +31,7 @@ class Tests:
         'test_data_1/gender_5_users.json',
         'test_data_1/gender_1_user.json',
         # 'test_data_1/gender_0_users.json',
-        # 'test_data_1/gender_15_users.json'
+        'test_data_1/gender_15_users.json'
     ])
     def test_count_by_country(self, base):
         data = read_json(base)
